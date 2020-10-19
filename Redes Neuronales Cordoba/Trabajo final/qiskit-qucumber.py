ed1c4bc8032eb38dd56fdf5cde82468b7a505484
agustinsilva447 <agustinsilva447@gmail.com>
2020-10-19 10:33:13 -0300

    fourier phi y data

--- Redes Neuronales Cordoba/Trabajo final/qiskit-qucumber.py
+++ Redes Neuronales Cordoba/Trabajo final/qiskit-qucumber.py
@@ -1,5 +1,6 @@
 import qucumber
 import numpy as np
+import matplotlib.pyplot as plt
 from qiskit import QuantumCircuit, execute, Aer
 from qiskit.visualization import plot_histogram, plot_bloch_multivector
 
@@ -20,15 +21,39 @@ def swap_registers(circuit, n):
 def qft(circuit, n):
     qft_rotations(circuit, n)
     swap_registers(circuit, n)
+
     return circuit
 
 n_qubits = 4
-qc = QuantumCircuit(n_qubits)
+qc = QuantumCircuit(n_qubits,n_qubits)
 qft(qc,n_qubits)
-print(qc)
 
 backend = Aer.get_backend('statevector_simulator')
 result = execute(qc, backend=backend).result()    
 statevector = result.get_statevector(qc)
-print("State vector: {}".format(statevector))
+print("State vector: {}".format(statevector.real))
+
 np.savetxt("phi_fourier.txt", statevector, fmt='%1.5f %1.5f')
+fig1 = plt.bar(np.arange(np.power(2,n_qubits)),np.power(statevector.real,2))
+plt.xlabel("Probabilities")
+plt.savefig("state.jpg")
+
+qc.measure(np.arange(n_qubits), np.arange(n_qubits)) 
+num_medidas = 1000
+backend = Aer.get_backend('qasm_simulator')
+result = execute(qc, backend=backend, shots=num_medidas).result()
+counts = result.get_counts(qc)
+print("Counts: {}".format(counts))
+
+mediciones = np.zeros([num_medidas,1])
+num_medidas = 0
+for medida, cantidad in counts.items():
+    mediciones[num_medidas:num_medidas + cantidad] = medida
+    num_medidas += cantidad
+np.random.shuffle(mediciones)
+
+np.savetxt("data_fourier.txt", mediciones, fmt='%04i')
+fig2 = plot_histogram(counts)
+fig2.savefig("data.jpg")
+
+print(qc)
