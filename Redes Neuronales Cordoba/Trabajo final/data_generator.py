8e7b6e3a110f1bb936aaf6463c151e7ba6b203fa
agustinsilva447 <agustinsilva447@gmail.com>
2020-10-26 12:18:41 -0300

    data generation n_qubits

--- Redes Neuronales Cordoba/Trabajo final/data_generator.py
+++ Redes Neuronales Cordoba/Trabajo final/data_generator.py
@@ -1,4 +1,3 @@
-import qucumber
 import numpy as np
 import matplotlib.pyplot as plt
 
@@ -24,10 +23,9 @@ def swap_registers(circuit, n):
 def qft(circuit, n):
     qft_rotations(circuit, n)
     swap_registers(circuit, n)
-
     return circuit
 
-n_qubits = 5
+n_qubits = 8
 qc = QuantumCircuit(n_qubits,n_qubits)
 qft(qc,n_qubits)
 
@@ -39,11 +37,11 @@ print("State vector: {}".format(statevector.real))
 np.savetxt("phi_fourier.txt", statevector, fmt='%1.5f %1.5f')
 fig1 = plt.bar(np.arange(np.power(2,n_qubits)),np.power(statevector.real,2))
 plt.xlabel("Probabilities")
-plt.ylim(0, 0.05)
+plt.ylim(0, 0.01)
 plt.savefig("state.jpg")
 
 qc.measure(np.arange(n_qubits), np.arange(n_qubits)) 
-num_medidas = 1000
+num_medidas = 1500
 
 backend1 = Aer.get_backend('qasm_simulator')
 result = execute(qc, backend=backend1, shots=num_medidas).result()
@@ -53,7 +51,7 @@ print("Counts: {}".format(counts))
 """
 IBMQ.save_account('bc0221c2c435408a718744a0e3388325cd4241375e15beefb8909a759a28201db7c6a425e8d07e09e54105e373aceccb175fa8eb46700b4db88ac50dbdc6fa84')
 provider = IBMQ.load_account()
-backend2 = least_busy(provider.backends(filters=lambda b: b.configuration().n_qubits >= 3 and not b.configuration().simulator and b.status().operational==True))
+backend2 = least_busy(provider.backends(filters=lambda b: b.configuration().n_qubits >= n_qubits and not b.configuration().simulator and b.status().operational==True))
 result = execute(qc, backend=backend2, shots=num_medidas).result()
 counts = result.get_counts(qc)
 print("Counts: {}".format(counts))
@@ -76,17 +74,16 @@ fin = open("data_fourier.txt", "wt")
 fin.write(data)
 fin.close()
 
-#fig2 = plot_histogram(counts)
-#fig2.savefig("state_hist.jpg")
-
 result = counts.values()
-data = list(result) 
-numpyArray = np.array(data) / num_medidas
+data1 = list(result) 
+numpyArray = np.array(data1) / num_medidas
 
-plt.close()
-fig2 = plt.bar(np.arange(np.power(2,n_qubits)),numpyArray)
-plt.xlabel("Probabilities")
-plt.ylim(0, 0.05)
-plt.savefig("state_hist.jpg")
-
-print(qc)
+if (np.arange(np.power(2,n_qubits)).shape == numpyArray.shape):
+    plt.close()
+    fig2 = plt.bar(np.arange(np.power(2,n_qubits)),numpyArray)
+    plt.xlabel("Probabilities")
+    plt.ylim(0, 0.01)
+    plt.savefig("state_hist.jpg")
+    print(qc)
+else:
+    print("\nHay estados con que no tienen mediciones :(. Repetir ejecución!")
